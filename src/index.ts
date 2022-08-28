64cad8cfffef07a6532d96dc954d904dc30534ce
猴子 <a102009102009@gmail.com>
2022-08-28 14:51:24 +0800

    feat error hand

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { Client } from "./client";
 
 console.clear();
 
+process
+  .on("uncaughtException", (er: Error) => console.error(er.toString()))
+  .on("unhandledRejection", (er: Error) => console.error(er.toString()));
+
 new Client().start();
