c62671f49aa320159029a4d438280a6b36079f6f
猴子 <a102009102009@gmail.com>
2022-08-29 09:41:48 +0800

    feat NGROK

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import { Client } from "./client";
 
 console.clear();
@@ -6,4 +8,19 @@ process
   .on("uncaughtException", (er: Error) => console.error(er.toString()))
   .on("unhandledRejection", (er: Error) => console.error(er.toString()));
 
-new Client().start();
+const client = new Client();
+client.start();
+
+const {
+  env: { NGROK_URL },
+} = process;
+
+if (process.env.NGROK || NGROK_URL) {
+  const setUrl = () => {
+    axios
+      .get(NGROK_URL || "line-and-discord:5000")
+      .then(({ data }) => client.line.setWebhookEndpointUrl(data));
+  };
+  setUrl();
+  setInterval(setUrl, 1e3 * 60 * 60); // 1 hour
+}
