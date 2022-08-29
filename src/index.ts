4e3096ef7627892fa6d66d2552b74fa8f022e8d9
猴子 <a102009102009@gmail.com>
2022-08-29 10:26:51 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,10 @@ const {
 
 if (process.env.NGROK || NGROK_URL) {
   const setUrl = () => {
-    axios
-      .get(NGROK_URL || "ngrok:1333")
-      .then(({ data }) => client.line.setWebhookEndpointUrl(data));
+    axios.get(NGROK_URL || "ngrok:1333").then(({ data }) => {
+      client.line.setWebhookEndpointUrl(data);
+      console.log("Webhook endpoint set to:", data);
+    });
   };
   setUrl();
   setInterval(setUrl, 1e3 * 60 * 60); // 1 hour
